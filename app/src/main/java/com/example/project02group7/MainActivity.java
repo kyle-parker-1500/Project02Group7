08f611079893bcefba8312bd57ee8b22581448e9
aleHernandez9725 <alexh1238@gmail.com>
2025-11-16 16:59:02 -0800

    got the navi menu working .

--- app/src/main/java/com/example/project02group7/MainActivity.java
+++ app/src/main/java/com/example/project02group7/MainActivity.java
@@ -7,6 +7,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,12 +17,44 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Find the BottomNavigationView
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView2);
+
+        // Create fragment instances
+        Fragment firstFragment = new HomeFragment();
+        Fragment secondFragment = new RecipeFragment();
+        Fragment thirdFragment = new AccountFragment();
+        Fragment fourthFragment = new SettingFragment();
+
+        // Set initial fragment
+        setCurrentFragment(firstFragment);
+        bottomNavigationView.setSelectedItemId(R.id.home);
+
+        // Handle bottom navigation selection using if–else (to avoid "constant expression required")
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+
+            if (id == R.id.home) {
+                setCurrentFragment(firstFragment);
+            } else if (id == R.id.recipe) {
+                setCurrentFragment(secondFragment);
+            } else if (id == R.id.profile) {
+                setCurrentFragment(thirdFragment);
+            } else if (id == R.id.setting) {
+                setCurrentFragment(fourthFragment);
+            }
+            return true;
         });
     }
+
+    // Helper method to switch fragments
+    private void setCurrentFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.flFragment, fragment)
+                .commit();
+    }
+
 }
